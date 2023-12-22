bab2a9c317c70aa491c5f87650960e2ca28cdb16
pat <starkidgleek25@gmail.com>
2023-12-22 16:05:49 +0800

    feat: add initial logging to file

--- utils/constants.py
+++ utils/constants.py
@@ -4,6 +4,8 @@ HOST_PORT=65500
 HOSTNAME='127.0.0.1'
 
 LOGGING_CONFIG = {
+    'filename': f"logs/test",
+    'filemode': 'a',
     'format': '%(asctime)s [%(levelname)s] - %(name)s: %(message)s',
     'datefmt': '%Y-%m-%dT%H:%M:%S%z',
     'level': logging._nameToLevel['DEBUG']
